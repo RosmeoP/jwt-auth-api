5cb3257ea5c0f7e3f233d1f79d6ea3e59ca6cfaf
Mauricio <vrosmeo@gmail.com>
2025-05-24 22:19:18 -0600

    Update Swagger configuration for improved API documentation

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -1,5 +1,5 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const options = {
   definition: {
@@ -23,18 +23,11 @@ const options = {
         },
       },
     },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
+    security: [{ bearerAuth: [] }],
   },
-  apis: ['./routes/*.js', './server.js'],
+  apis: ['./routes/*.js', './server.js'], 
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
-module.exports = {
-  swaggerUi,
-  swaggerSpec,
-};
+export { swaggerUi, swaggerSpec };
